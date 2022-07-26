a95df8076d08eaba0eb2f671878049a19301b048
Josimar Souza <contact.josimarsouza@gmail.com>
2022-07-26 15:42:04 -0300

    Added register controller to the user router

--- controller/users/userRouter.js
+++ controller/users/userRouter.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const registerUser = require('./register');
 
 const userRouter = express.Router({ mergeParams: true });
+
+userRouter.post('/register', registerUser);
 
 module.exports = (rootRouter) => {
   rootRouter.use('/user', userRouter);
