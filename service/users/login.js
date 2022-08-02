0c739a8664e11dd924f65e71b91b607ed5e8f213
Josimar Souza <contact.josimarsouza@gmail.com>
2022-08-02 14:29:14 -0300

    Created service for login

--- service/users/login.js
+++ service/users/login.js
@@ -1,8 +1,50 @@
+const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const ErrorCreator = require('../../helpers/errorCreator');
 const userModels = require('../../model/users');
+const userValidations = require('../../validations/users');
 
-const login = async () => {
-  try {
+dotenv.config();
+
+const { TOKEN_PRIVATE_KEY } = process.env;
+
+const getToken = (user) => {
+  const { email, username } = user
+  const tokenOptions = {
+    algorithm: 'HS256',
+    expiresIn: '1h',
+  }
   
+  const paylaod = {
+    email,
+    username,
+  };
+  
+  const token = jwt.sign(paylaod, TOKEN_PRIVATE_KEY, tokenOptions);
+  
+  return token;
+}
+
+const login = async (user) => {
+  try {
+    const validationResult = userValidations.login(user);
+    
+    if ('error' in validationResult) {
+      const error = new ErrorCreator(validationResult.error.message, StatusCodes.BAD_REQUEST);
+      return error;
+    }
+    
+    const userFounded = await userModels.login(user);
+    
+    if (!userFounded.email) {
+      const error = new ErrorCreator('User not founded', StatusCodes.BAD_REQUEST);
+      return error;
+    }
+    
+    const token = getToken(userFounded);
+    
+    return token;
   } catch (error) {
   
   }
--- validations/users/index.js
+++ validations/users/index.js
@@ -1,7 +1,9 @@
 const register = require('./register');
+const login = require('./login');
 
 const usersValidations = {
   register,
+  login,
 };
 
 module.exports = usersValidations;
