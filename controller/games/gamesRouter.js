fc21f2a37abdb4e9a420efe7cb806ae2390df130
Josimar Souza <contact.josimarsouza@gmail.com>
2022-08-07 16:48:12 -0300

    Created middleware for authorization

--- controller/games/gamesRouter.js
+++ controller/games/gamesRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const addGame = require('./addGame');
+const Auth = require('../../middlewares/Auth');
 
 const gamesRouter = express.Router({ mergeParams: true });
 
-gamesRouter.post('/', addGame);
+gamesRouter.post('/', Auth, addGame);
 
 module.exports = (rootRouter) => {
   rootRouter.use('/games', gamesRouter);
