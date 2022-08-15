92c5bb89e60d7ebbc1051df1c92f93ed588344e0
Josimar Souza <contact.josimarsouza@gmail.com>
2022-08-15 16:54:42 -0300

    Created service to delete a game

---
+++ service/games/deleteGameById.js
@@ -0,0 +1,23 @@
+const { StatusCodes } = require('http-status-codes');
+const gamesModels = require('../../model/games');
+const ErrorCreator = require('../../helpers/errorCreator');
+
+const deleteGameById = async (id, user) => {
+  const game = await gamesModels.findGameById(id);
+  
+  if (!game) {
+    const error = new ErrorCreator('Game not found!', StatusCodes.NOT_FOUND);
+    return error;
+  }
+  
+  if (game.user !== user.email) {
+    const error = new ErrorCreator('You can only delete games that you posted!', StatusCodes.UNAUTHORIZED);
+    return error;
+  }
+  
+  const deletedGame = await gamesModels.deleteGameById(id);
+  
+  return deletedGame;
+};
+
+module.exports = deleteGameById;
