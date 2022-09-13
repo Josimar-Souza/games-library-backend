b12e7ebf180bd493fa4b3f82f97cdd2497fe9f46
Josimar Souza <contact.josimarsouza@gmail.com>
2022-09-13 16:06:33 -0300

    Created updateGameById controller

--- controller/games/gamesRouter.js
+++ controller/games/gamesRouter.js
@@ -2,11 +2,14 @@ const express = require('express');
 const addGame = require('./addGame');
 const getUserGames = require('./getUserGames');
 const deleteGameById = require('./deleteGameById');
+const updateGameById = require('./updateGameById');
 const Auth = require('../../middlewares/Auth');
+const { updateGame } = require('../../validations/games');
 
 const gamesRouter = express.Router({ mergeParams: true });
 
 gamesRouter.post('/', Auth, addGame);
+gamesRouter.post('/:id', Auth, updateGame);
 gamesRouter.get('/', Auth, getUserGames);
 gamesRouter.delete('/:id', Auth, deleteGameById);
 
---
+++ controller/games/updateGameById.js
@@ -0,0 +1,19 @@
+const { StatusCodes } = require('http-status-codes');
+const ErrorCreator = require('../../helpers/errorCreator');
+const gamesService = require('../../service/games');
+
+const updateGameById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const updatedGame = await gamesService.updateGameById(id, req.body);
+
+    if (updatedGame instanceof ErrorCreator) {
+      return res.status(updatedGame.status).json({ message: updatedGame.message });
+    }
+
+    return res.status(StatusCodes.OK).json({ updatedGame });
+  } catch (error) {
+    next(error);
+  }
+}
