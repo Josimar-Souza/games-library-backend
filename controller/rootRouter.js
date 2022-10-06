1a945ccb7ae65b3c362dedf12dea8fc9f93268c4
Josimar Souza <contact.josimarsouza@gmail.com>
2022-10-06 16:25:39 -0300

    Added home route

--- controller/rootRouter.js
+++ controller/rootRouter.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const userRouter = require('./users/userRouter');
 const gamesRouter = require('./games/gamesRouter');
+const { StatusCodes } = require('http-status-codes');
 
 const rootRouter = express.Router({ mergeParams: true });
+
+rootRouter.get('/', (_req, res, next) => {
+  try {
+    return res.status(StatusCodes.OK).json({ message: 'Server is online' });
+  } catch (error) {
+    next(error);
+  }
+})
 
 userRouter(rootRouter);
 gamesRouter(rootRouter);
