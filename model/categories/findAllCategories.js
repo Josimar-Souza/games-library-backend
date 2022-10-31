7ee8d6619a93ddd23aa4b123f4cbbce16952a90d
Josimar Souza <contact.josimarsouza@gmail.com>
2022-10-31 15:23:52 -0300

    Created findAllCategories model

---
+++ model/categories/findAllCategories.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+const getDBConnection = require('../connection');
+
+dotenv.config();
+const { DB_CATEGORIES_COLLECTION } = process.env;
+
+const findAllCategories = async (user) => {
+  const db = await getDBConnection();
+
+  const categories = await db.collection(DB_CATEGORIES_COLLECTION).findMany({ user: user.email }).toArray();
+
+  return categories;
+};
+
+module.exports = findAllCategories;
