6158ab3530d592e53fca4a505f2ec58099f91d1d
Josimar Souza <contact.josimarsouza@gmail.com>
2022-10-31 15:39:50 -0300

    Created findAllCategories controller

--- controller/categories/categoriesRouter.js
+++ controller/categories/categoriesRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const Auth = require('../../middlewares/Auth');
 const addCategory = require('./addCategory');
+const findAllCategories = require('./findAllCategories');
 
 const categoriesRouter = Router({ mergeParams: true });
 
 categoriesRouter.post('/', Auth, addCategory);
+categoriesRouter.get('/', Auth, findAllCategories);
 
 module.exports = (rootRouter) => {
   rootRouter.use('/category', categoriesRouter);
--- model/categories/findAllCategories.js
+++ model/categories/findAllCategories.js
@@ -7,7 +7,7 @@ const { DB_CATEGORIES_COLLECTION } = process.env;
 const findAllCategories = async (user) => {
   const db = await getDBConnection();
 
-  const categories = await db.collection(DB_CATEGORIES_COLLECTION).findMany({ user: user.email }).toArray();
+  const categories = await db.collection(DB_CATEGORIES_COLLECTION).find({ user: user.email }).toArray();
 
   return categories;
 };
