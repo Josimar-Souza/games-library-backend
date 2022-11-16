c7318c8a2673f1da10dcf6c70d44c5770436ce25
Josimar Souza <contact.josimarsouza@gmail.com>
2022-11-16 15:39:47 -0300

    Category length changed and readme file updated

--- validations/categories/addCategory.js
+++ validations/categories/addCategory.js
@@ -1,7 +1,7 @@
 const joi = require('joi');
 
 const addCategorySchema = joi.object({
-  category: joi.string().min(3).max(20).required()
+  category: joi.string().min(3).max(35).required()
 });
 
 module.exports = (value) => addCategorySchema.validate(value);
